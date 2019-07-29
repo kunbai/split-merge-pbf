a5603cabc304d9537f71a91ac361b851ed24f774
“Kunbai <kunbai@gamevil.com>
2019-07-29 12:11:11 +0900

    fix

--- makeit.js
+++ makeit.js
@@ -195,7 +195,7 @@ async.waterfall([
           // console.log('!!!!!!' + outputFile)
 
 
-          var listFileName = 'list' - info.movieFileName + '.txt'
+          var listFileName = 'list' + info.pureFileName + '.txt'
           var listFilePath = path.join(outputPath, listFileName)
 
           info.splitInfo.forEach((spInfo) => {
